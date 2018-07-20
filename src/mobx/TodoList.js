01f102a228f25cda7a0abf057cac04abd7035d66
pigflymoon <pplflymoon2015@gmail.com>
2018-07-20 19:52:43 +1200

    todo list

--- src/mobx/TodoList.js
+++ src/mobx/TodoList.js
@@ -2,7 +2,28 @@ import React from "react"
 import {observer} from 'mobx-react'
 @observer
 export default class TodoList extends React.Component {
+    
+   createNew = (e) => {
+       if(e.which === 13){
+           this.props.store.createTodo(e.target.value)
+           e.target.value = ""
+       }
+   }
+    filter = (e) => {
+        this.props.store.filter = e.target.value
+    }
+    
     render() {
-        return <h1>{this.props.store.todos[0]}</h1>
+        const {filter, todos,filteredTodos} =this.props.store
+        const todoList = filteredTodos.map((todo,i) => (
+            <li key={i}>{todo}</li>
+        ))
+        return <div>
+            <h1>todos</h1>
+            <div>{filter}</div>
+            <input className="create" onKeyPress={this.createNew}/>
+            <input className="filter" value={filter} onChange={this.filter}/>
+            <ul>{todoList}</ul>
+        </div>
     }
 }
--- src/mobx/TodoStore.js
+++ src/mobx/TodoStore.js
@@ -1,16 +1,18 @@
-import {autorun, observable} from "mobx"
+import {computed, observable} from "mobx"
 
 
 export class TodoStore {
     @observable todos = ["buy apple", "buy orange"]
     @observable filter = ""
+    @computed get filteredTodos(){
+        var matchesFilter = new RegExp(this.filter,"i")
+        return this.todos.filter(todo=>!this.filter || matchesFilter.test(todo))
+    }
+
+    createTodo = (value) => {
+        this.todos.push(value)
+    }
 
 }
 
-var store = window.store = new TodoStore()
-export default store
-
-autorun(()=>{
-    console.log(store.filter);
-    console.log(store.todos[0]);
-})
+export default new TodoStore()
